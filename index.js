b8a669d6645af3f24962f4a3f724d3082c068a62
victornsouza1 <victornsouza1@gmail.com>
2025-04-13 20:23:01 -0300

    Primeiro Commit

---
+++ index.js
@@ -0,0 +1,20 @@
+let nomeDoHeroi = "Taaz"
+let xpDoHeroi = 10001
+
+if (xpDoHeroi <= 1000){
+    console.log("O herói de nome " + nomeDoHeroi + " está no nível de Ferro")
+} else if (xpDoHeroi <= 2000){
+    console.log("O herói de nome " + nomeDoHeroi + " está no nível de Bronze")
+} else if (xpDoHeroi <= 5000){
+    console.log("O herói de nome " + nomeDoHeroi + " está no nível de Prata")
+} else if (xpDoHeroi <= 7000){
+    console.log("O herói de nome " + nomeDoHeroi + " está no nível de Ouro")
+} else if (xpDoHeroi <= 8000){
+    console.log("O herói de nome " + nomeDoHeroi + " está no nível de Platina")
+} else if (xpDoHeroi <= 9000){
+    console.log("O herói de nome " + nomeDoHeroi + " está no nível de Ascendente")
+} else if (xpDoHeroi <= 10000){
+    console.log("O herói de nome " + nomeDoHeroi + " está no nível de Imortal")
+} else {
+    console.log("O herói de nome " + nomeDoHeroi + " está no nível de Radiante")
+}
